3f8c057da3d74b389da315dfae9794122b2fb8b6
tutosrive <tutosrivegamer@gmail.com>
2025-05-29 22:12:16 -0500

    Update home, filter repos section

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -17,22 +17,31 @@ async function loadDescription() {
  */
 async function loadRepos() {
     const container = document.querySelector('#carrusel-repos');
+    const configsWeb = await loadConfigs();
     try {
         const res = await fetch('https://api.github.com/orgs/dev2forge/repos');
         const repos = await res.json();
         repos.forEach((repo) => {
-            const card = document.createElement('div');
-            card.className = 'flex flex-col justify-between items-center w-64 h-64 bg-gray-100 dark:bg-gray-800 p-4 rounded shadow hover:shadow-lg transition snap-start row-span-1';
-            card.innerHTML = `
+            // Skip repositories that are not in the configsWeb list
+            if (!configsWeb.ignoreRepos.includes(repo.name)) {
+                const card = document.createElement('div');
+                card.className = 'flex flex-col justify-between items-center w-64 h-64 bg-gray-100 dark:bg-gray-800 p-4 rounded shadow hover:shadow-lg transition snap-start row-span-1';
+                card.innerHTML = `
                 <h2 class="text-xl font-semibold text-center w-full mb-2">${repo.name}</h2>
                 <div class="flex-1 w-full overflow-y-auto mb-2 no-scrollbar">
                     <p class="text-sm text-gray-600 dark:text-gray-300 text-center break-words">${repo.description || 'No description'}</p>
                 </div>
-                <div class="w-full flex justify-center mt-auto">
-                    <a href="${repo.html_url}" target="_blank" class="btn-repo-link inline-block text-center w-full">View Repo</a>
+                <div class="w-full flex justify-center mt-auto gap-3">
+                    <a href="${repo.html_url}" target="_blank" class="btn-repo-link flex items-center justify-center w-12 h-12 rounded-full bg-blue-100 dark:bg-blue-900 shadow hover:scale-110 hover:bg-blue-500 hover:text-white transition border border-blue-300 dark:border-blue-700 mr-0.5">
+                        <img width="24" height="24" src="https://cdn.jsdelivr.net/gh/tutosrive/images-projects-srm-trg@refs/heads/main/svg-icons-flags-cursor/icon/github-logo.svg" alt="GitHub">
+                    </a>
+                    <a href="${repo.homepage}" target="_blank" class="btn-repo-link flex items-center justify-center w-12 h-12 rounded-full bg-blue-100 dark:bg-blue-900 shadow hover:scale-110 hover:bg-blue-500 hover:text-white transition border border-blue-300 dark:border-blue-700 ml-0.5">
+                        <img width="16" height="16" src="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/icons/box-arrow-up-right.svg" alt="Icon redirect">
+                    </a>
                 </div>
             `;
-            container.appendChild(card);
+                container.appendChild(card);
+            }
         });
     } catch (e) {
         container.innerHTML = '<p class="text-red-500">Failed to load repositories.</p>';
@@ -56,4 +65,10 @@ async function loadFollowers() {
         img.onclick = () => window.open(follower.html_url, '_blank');
         container.appendChild(img);
     });
+}
+
+async function loadConfigs() {
+    const req = await fetch('src/assets/json/configs.json');
+    const data = await req.json();
+    return data;
 }
